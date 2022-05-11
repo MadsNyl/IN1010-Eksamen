9699b9a02159284b72e124857a661781870571de
Mads Nylund <madsnyl@gmail.com>
2022-05-11 14:15:58 +0200

    oppgave 4 ferdig

--- Boilerplate/Traader/Oblig5/Hovedprogram.java
+++ Boilerplate/Traader/Oblig5/Hovedprogram.java
@@ -2,11 +2,15 @@ import java.util.HashMap;
 
 public class Hovedprogram {
     public static void main(String[] args) {
-        String filvei = "Data/test1.txt";
+        String filvei1 = "Data/test1.txt";
+        String filvei2 = "Data/test2.txt";
 
-        HashMap<String, Subsekvens> kart = SubsekvensRegister.lesFil(filvei);
+        HashMap<String, Subsekvens> kart1 = SubsekvensRegister.lesFil(filvei1);
+        HashMap<String, Subsekvens> kart2 = SubsekvensRegister.lesFil(filvei2);
 
-        for (String e : kart.keySet()) {
+        HashMap<String, Subsekvens> flettet_kart = SubsekvensRegister.flett(kart1, kart2);
+
+        for (Subsekvens e : flettet_kart.values()) {
             System.out.println(e);
         }
     }
--- Boilerplate/Traader/Oblig5/SubsekvensRegister.java
+++ Boilerplate/Traader/Oblig5/SubsekvensRegister.java
@@ -29,9 +29,7 @@ public class SubsekvensRegister {
                         char del1 = linje.charAt(i);
                         char del2 = linje.charAt(i + 1);
                         char del3 = linje.charAt(i + 2);
-                        streng += del1;
-                        streng += del2;
-                        streng += del3;
+                        streng = "" + del1 + del2 + del3;
                         Subsekvens subsekvens = new Subsekvens(streng);
                         kart.put(streng, subsekvens);
                     }
@@ -41,8 +39,31 @@ public class SubsekvensRegister {
         } catch (FileNotFoundException e) { 
             e.printStackTrace();
         }
-
         return kart;
+    }
 
+    // slaar sammen to hashmaps til ett
+    static HashMap<String, Subsekvens> flett(HashMap<String, Subsekvens> kart1, HashMap<String, Subsekvens> kart2) {
+        HashMap<String, Subsekvens> retur_kart = new HashMap<>();
+
+        // itererer gjennom kart nummer to og fyller inn verdier inn i retur kart
+        for (Subsekvens x : kart2.values()) {
+            retur_kart.put(x.hentSubsekvens(), x);
+        }
+
+        // itererer gjennom forste kart og sammenligner med verdier fra andre kart
+        for (Subsekvens x : kart1.values()) {
+            int teller = 0;
+            for (Subsekvens y : kart2.values()) {
+                if (x.hentSubsekvens().equals(y.hentSubsekvens())) {
+                    teller = x.hentAntall() + y.hentAntall();
+                }
+            }
+            Subsekvens ny_subsekvens = new Subsekvens(x.hentSubsekvens());
+            if (teller > 0) ny_subsekvens.endreAntall(teller);
+            retur_kart.put(x.hentSubsekvens(), ny_subsekvens);
+        }
+
+        return retur_kart;
     }
 }
